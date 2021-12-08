473a946b509e92bd323eb46c4338d104c432c660
Paulo Henrique Silva <pshenrique2015@gmail.com>
2021-12-08 09:56:57 -0300

    Player vs Player game is working

--- mainfile.c
+++ mainfile.c
@@ -2,25 +2,20 @@
 //It's totally free to use 
 //github: Paulo-Henrique-Silva
 
-/*
-In this version the Game Works, but: 
-    - it's not going to stop if it's a tie 
-    - it does not tell who has won
-
-# Add this later
-*/
 
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
 
+char board[9];
+char winnerSymbol;
+
 void createBoard();
 int isValid(int input);
 void refreshBoard(char playerSymbol, int play);
 int isVictory(char symb);
+int isTie();
 //fuctions prototypes
-
-char board[9];
 
 int main()
 {
@@ -36,7 +31,7 @@ int main()
         play = isValid(play);
         refreshBoard(PLAYER1, play);
 
-        if(isVictory(PLAYER1))
+        if(isVictory(PLAYER1) || isTie())
             break; 
 
         printf("\n\nPlayer 2 Turn - Type your play(1 - 9): ");
@@ -44,18 +39,25 @@ int main()
         play = isValid(play);
         refreshBoard(PLAYER2, play);
 
-        if(isVictory(PLAYER2))
+        if(isVictory(PLAYER2) || isTie())
             break; 
     }
 
     printf("\n\nEND GAME!");
+
+    if(winnerSymbol == 'X')
+        printf("\nPALYER 1 WINS");
+    else if(winnerSymbol == 'O')
+        printf("\nPALYER 2 WINS");
+    else
+        printf("\nIT'S A TIE!");
+
     getch(); //pause before closes it
     return 0;
 }
 
 void createBoard()
 {
-
     for(int i = 0; i < 9; i++)
         board[i] = (i+1) + '0'; 
     //add '0' will assing the "i" value as a char
@@ -66,6 +68,8 @@ void createBoard()
     printf("\n %c | %c | %c ", board[3], board[4], board[5]);
     printf("\n---|---|---");
     printf("\n %c | %c | %c ", board[6], board[7], board[8]);
+
+    winnerSymbol = ' '; //sets the winner symbol, because anyone has won.
 }
 //it creates the game board
 
@@ -97,27 +101,46 @@ void refreshBoard(char playerSymbol, int play)
 int isVictory(char symb)
 {
     //rows
-    if(board[0] == symb && board[1] == symb && board[2] == symb)
-        return 1;
-    if(board[3] == symb && board[4] == symb && board[5] == symb)
-        return 1;
-    if(board[6] == symb && board[7] == symb && board[8] == symb)
-        return 1;
+    if
+    (
+        (board[0] == symb && board[1] == symb && board[2] == symb) ||   
+        (board[3] == symb && board[4] == symb && board[5] == symb) ||    
+        (board[6] == symb && board[7] == symb && board[8] == symb)
+    )
+        winnerSymbol = symb;
 
     //columns
-    if(board[0] == symb && board[3] == symb && board[6] == symb)
-        return 1;
-    if(board[1] == symb && board[4] == symb && board[7] == symb)
-        return 1;
-    if(board[2] == symb && board[5] == symb && board[8] == symb)
-        return 1;
+    if
+    (
+        (board[0] == symb && board[3] == symb && board[6] == symb) ||
+        (board[1] == symb && board[4] == symb && board[7] == symb) ||
+        (board[2] == symb && board[5] == symb && board[8] == symb)
+    )
+        winnerSymbol = symb;
 
     //diagonals
-    if(board[0] == symb && board[4] == symb && board[8] == symb)
-        return 1;
-    if(board[2] == symb && board[4] == symb && board[6] == symb)
-        return 1;
+    if
+    (
+        (board[0] == symb && board[4] == symb && board[8] == symb) ||
+        (board[2] == symb && board[4] == symb && board[6] == symb)
+    )
+        winnerSymbol = symb;
 
-    return 0; //if every case don't matches, will return false. Therefore, anyone hasn't won.
+    if(winnerSymbol != ' ')
+        return 1;
+    else
+        return 0; //if every case don't matches, will return false. Therefore, anyone hasn't won.
 }
 //checks if it's a victory
+
+int isTie()
+{
+    for(int i = 0; i < 9; i++)
+    {
+        if(board[i] != 'X' && board[i] != 'O') //it means that the game hasn't finish yet
+            return 0;
+    }
+
+    return 1; //if it's a tie, returns TRUE 
+}
+
